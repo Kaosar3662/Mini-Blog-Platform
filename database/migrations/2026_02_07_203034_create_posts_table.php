e6057a2e62c75a76a9bf79de91ad1eb6fa930290
Kaosar Ali <kaosar3662@gmail.com>
2026-02-08 02:51:39 +0600

    Forget To add this files

---
+++ database/migrations/2026_02_07_203034_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->restrictOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('short_desc');
+            $table->longText('content');
+            $table->string('cover_image')->nullable();
+            $table->enum('status', ['draft', 'pending', 'approved', 'rejected'])->default('draft');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
